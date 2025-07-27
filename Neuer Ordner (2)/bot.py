e822e720d10596912d1676f78dc34ae086e34c05
Ayman5552 <manhalyt.187@gmail.com>
2025-07-27 21:39:15 +0200

    Update bot.py

--- Neuer Ordner (2)/bot.py
+++ Neuer Ordner (2)/bot.py
@@ -1,3 +1,14 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def keep_alive():
+    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
 import nest_asyncio
 nest_asyncio.apply()
 
