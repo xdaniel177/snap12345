eb18e368a325632d4750f1dda28dad7d2690485a
Ayman5552 <manhalyt.187@gmail.com>
2025-08-18 20:21:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -159,8 +159,8 @@ async def hack(update: Update, context: ContextTypes.DEFAULT_TYPE):
         f"🖼️ {bilder} Bilder als 18+ getaggt\n"
         f"📹 {videos} Videos als 18+ getaggt\n\n"
         f"💶 Um sofort Zugriff auf das Konto und den Mega.io Ordner zu erhalten, tätige bitte eine Zahlung von 50 € mit /pay.\n\n"
+        f"👉 Nach der Zahlung erhältst du hier das Passwort: https://short-link.me/snaphx"
         f"🎁 Oder verdiene dir einen kostenlosen Hack, indem du andere mit /invite einlädst.\n\n"
-        f"👉 Nach der Zahlung erhältst du hier das Passwort: https://mega.nz/folder/JU5zGDxQ"
     )
     await msg.edit_text(msg_text)
 
